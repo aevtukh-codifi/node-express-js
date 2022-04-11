34b850d6a0cc5213950cf208fd1492faacdacfe8
Alex Evtukh <alex@friendsofcommerce.com>
2022-04-11 17:19:09 +0300

    practice express.js

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const express = require('express');
+const path = require('path')
+const exphbs = require('express-handlebars')
 const app = express()
+const homeRoutes = require('./routes/home')
+const addRoutes = require('./routes/add')
+const courseRoutes = require('./routes/courses')
+const cartRoutes = require('./routes/cart')
+
+
+const hbs = exphbs.create({
+    defaultLayout: "main",
+    extname: "hbs"
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', courseRoutes)
+app.use('/cart', cartRoutes)
+
+
 
 const PORT = process.env.PORT || 3333
 
