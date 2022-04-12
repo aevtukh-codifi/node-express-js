6d3eac072bbaba5cdc388f12ccc8aa83797c7237
Alex Evtukh <alex@friendsofcommerce.com>
2022-04-12 18:25:10 +0300

    paractice-mongodb

--- index.js
+++ index.js
@@ -1,34 +1,70 @@
-const express = require('express');
+const express = require('express')
 const path = require('path')
+const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const app = express()
 const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
-const courseRoutes = require('./routes/courses')
-const cartRoutes = require('./routes/cart')
+const ordersRoutes = require('./routes/orders')
+const coursesRoutes = require('./routes/courses')
+const User = require('./models/user')
 
+const app = express()
 
 const hbs = exphbs.create({
-    defaultLayout: "main",
-    extname: "hbs"
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
 
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs')
 app.set('views', 'views')
 
+app.use(async (req, res, next) => {
+  try {
+    const user = await User.findById('5cc1d29dcedab01481e03660')
+    req.user = user
+    next()
+  } catch (e) {
+    console.log(e)
+  }
+})
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.urlencoded({extended: true}))
 
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
-app.use('/courses', courseRoutes)
-app.use('/cart', cartRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
+app.use('/orders', ordersRoutes)
+
+const PORT = process.env.PORT || 5000
+
+async function start() {
+  try {
+    const url = `mongodb+srv://vladilen:0I5GEL9uLUcR38GC@cluster0-3rrau.mongodb.net/shop`
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    })
+    const candidate = await User.findOne()
+    if (!candidate) {
+      const user = new User({
+        email: 'vladilen@mail.ru',
+        name: 'Vladilen',
+        cart: {items: []}
+      })
+      await user.save()
+    }
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
 
 
-
-const PORT = process.env.PORT || 3333
-
-app.listen(PORT, () => {
-    console.log(`Server is running on PORT ${PORT}`)
-})
